40c887b62297837e9b2dcce0baf392abfe91e78a
Myles Carrick <myles@mylescarrick.com>
2012-05-02 16:29:40 +1000

    encoding fix for 1.9.3

--- lib/net/ber/core_ext/string.rb
+++ lib/net/ber/core_ext/string.rb
@@ -20,7 +20,7 @@ module Net::BER::Extensions::String
     if self.respond_to?(:encode)
       # Strings should be UTF-8 encoded according to LDAP.
       # However, the BER code is not necessarily valid UTF-8
-      self.encode('UTF-8').force_encoding('ASCII-8BIT')
+      self.force_encoding('ASCII-8BIT')
     else
       self
     end
